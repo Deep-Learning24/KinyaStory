b92442859eed94ca701e2bbefa10b7f27e404720
kclaudeeager <claudekwizera003@gmail.com>
2024-03-21 23:24:30 +0200

    using python3

--- setup.sh
+++ setup.sh
@@ -6,10 +6,10 @@ pip install -r requirements.txt
 
 # Step 2: Run DownloadKinyastory script
 echo "Running DownloadKinyastory..."
-python download_kinyastory.py
+python3 download_kinyastory.py
 
 # Step 3: Run KinyaTokenizer script
 echo "Running KinyaTokenizer..."
-python KinyaTokenizer.py
+python3 KinyaTokenizer.py
 
 echo "Setup completed successfully."
