9f89c5b25f0d7f2e6383b549efce0e31d60e9066
kclaudeeager <claudekwizera003@gmail.com>
2024-03-22 14:05:50 +0200

    Changing the wandb for better tracking

--- train_kinyastory.py
+++ train_kinyastory.py
@@ -91,9 +91,9 @@ rouge = Rouge()
 import wandb
 wandb.login(key="3644f3d76a394594794c1b136a20f75303e871ba")#API Key is in your wandb account, under settings (wandb.ai/settings)
 run = wandb.init(
-    name = "kinya-story", ## Wandb creates random run names if you skip this field
+    name = "kinya-story-new", ## Wandb creates random run names if you skip this field
     reinit = True, ### Allows reinitalizing runs when you re-run this cell
-    id ="kinya-story", ### Insert specific run id here if you want to resume a previous run
+    id ="kinya-story-new", ### Insert specific run id here if you want to resume a previous run
     #resume = "must", ### You need this to resume previous runs, but comment out reinit = True when using this
     project = "project-ablations", ### Project should be created in your wandb account
     config = config ### Wandb Config for your run
